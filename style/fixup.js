ff2e900b433579a98491e2ac801a31f258c31ea0
Mike Perlman <mac2net@me.com>
2016-07-09 13:26:43 +0200

    Update fixup.js

--- style/fixup.js
+++ style/fixup.js
@@ -4,38 +4,54 @@
  * This code handles some fixup to improve the table of contents.             *
  * It is intended to be a very simple script for 2016.                        *
  ******************************************************************************/
-(function(){
-  function toggleSidebar(on) {
+(function() {
+  "use strict";
+  var collapseSidebarText = '<span aria-hidden="true">←</span> '
+                          + '<span>Collapse Sidebar</span>';
+  var expandSidebarText   = '<span aria-hidden="true">→</span> '
+                          + '<span>Pop Out Sidebar</span>';
+  var tocJumpText         = '<span aria-hidden="true">↑</span> '
+                          + '<span>Jump to Table of Contents</span>';
 
-    /* Don't scroll to compensate for the ToC if we're above it already. */
-    var headY = 0;
-    var head = document.querySelector('.head');
-    headY += head.offsetTop + head.offsetHeight; // terrible approx of "top of ToC"
-    var skipScroll = window.scrollY < headY;
+  var sidebarMedia = window.matchMedia('screen and (min-width: 78em)');
+  var autoToggle   = function(e){ toggleSidebar(e.matches) };
+  if(sidebarMedia.addListener) {
+    sidebarMedia.addListener(autoToggle);
+  }
 
+  function toggleSidebar(on, skipScroll) {
     if (on == undefined) {
       on = !document.body.classList.contains('toc-sidebar');
     }
 
+    if (!skipScroll) {
+      /* Don't scroll to compensate for the ToC if we're above it already. */
+      var headY = 0;
+      var head = document.querySelector('.head');
+      if (head) {
+        // terrible approx of "top of ToC"
+        headY += head.offsetTop + head.offsetHeight;
+      }
+      skipScroll = window.scrollY < headY;
+    }
+
     var toggle = document.getElementById('toc-toggle');
-    var toggleAbbr = toggle.firstChild;
     var tocNav = document.getElementById('toc');
     if (on) {
       var tocHeight = tocNav.offsetHeight;
       document.body.classList.add('toc-sidebar');
       document.body.classList.remove('toc-inline');
-      toggleAbbr.textContent = "←";
-      toggleAbbr.title = "Collapse Sidebar";
+      toggle.innerHTML = collapseSidebarText;
       if (!skipScroll) {
         window.scrollBy(0, 0 - tocHeight);
       }
       tocNav.focus();
+      sidebarMedia.addListener(autoToggle); // auto-collapse when out of room
     }
     else {
       document.body.classList.add('toc-inline');
       document.body.classList.remove('toc-sidebar');
-      toggleAbbr.textContent = "→";
-      toggleAbbr.title = "Pop Out Sidebar";
+      toggle.innerHTML = expandSidebarText;
       if (!skipScroll) {
         window.scrollBy(0, tocNav.offsetHeight);
       }
@@ -51,17 +67,26 @@
     /* Create the sidebar toggle in JS; it shouldn't exist when JS is off. */
     var toggle = document.createElement('a');
       /* This should probably be a button, but appearance isn't standards-track.*/
-    toggle.setAttribute('id', 'toc-toggle');
-    toggle.setAttribute('class', 'toc-toggle');
-    toggle.setAttribute('href', '#toc');
-    toggle.addEventListener('click', function(e){ e.preventDefault(); toggleSidebar(); return false;}, false);
-    toggle.innerHTML = "<abbr title='Collapse Sidebar'>←</abbr>";
+    toggle.id = 'toc-toggle';
+    toggle.class = 'toc-toggle';
+    toggle.href = '#toc';
+    toggle.innerHTML = collapseSidebarText;
+
+    sidebarMedia.addListener(autoToggle);
+    var toggler = function(e) {
+      e.preventDefault();
+      sidebarMedia.removeListener(autoToggle); // persist explicit off states
+      toggleSidebar();
+      return false;
+    }
+    toggle.addEventListener('click', toggler, false);
+
 
     /* Get <nav id=toc-nav>, or make it if we don't have one. */
     var tocNav = document.getElementById('toc-nav');
     if (!tocNav) {
-      tocNav = document.createElement('nav');
-      tocNav.setAttribute('id', 'toc-nav');
+      tocNav = document.createElement('p');
+      tocNav.id = 'toc-nav';
       /* Prepend for better keyboard navigation */
       document.body.insertBefore(tocNav, document.body.firstChild);
     }
@@ -69,42 +94,50 @@
     var tocJump = document.getElementById('toc-jump');
     if (!tocJump) {
       tocJump = document.createElement('a');
-      tocJump.setAttribute('id', 'toc-jump');
-      tocJump.setAttribute('href', '#toc');
-      tocJump.innerHTML = "<abbr title='Jump to Table of Contents'>↑</abbr>";
+      tocJump.id = 'toc-jump';
+      tocJump.href = '#toc';
+      tocJump.innerHTML = tocJumpText;
       tocNav.appendChild(tocJump);
     }
 
     tocNav.appendChild(toggle);
   }
 
-  createSidebarToggle();
-  var sidebarMedia = window.matchMedia('screen and (min-width: 78em)');
-  if(sidebarMedia.addEventListener) {
-    sidebarMedia.addEventListener('change', function(e){toggleSidebar(e.matches);}, false);
-  } else if(sidebarMedia.addListener) {
-    sidebarMedia.addListener(function(e){toggleSidebar(e.matches);});
-  }
-  toggleSidebar(sidebarMedia.matches);
-
-  /* If the sidebar has been manually opened and is currently overlaying the text
-     (window too small for the MQ to add the margin to body),
-     then auto-close the sidebar once you click on something in there. */
-  document.getElementById('toc').addEventListener('click', function(e) {
-    if(e.target.tagName.toLowerCase() == "a" && document.body.classList.contains('toc-sidebar') && !sidebarMedia.matches) {
-      toggleSidebar();
+  var toc = document.getElementById('toc');
+  if (toc) {
+    if (!document.getElementById('toc-toggle')) {
+      createSidebarToggle();
     }
-  }, false);
+    toggleSidebar(sidebarMedia.matches, true);
+
+    /* If the sidebar has been manually opened and is currently overlaying the text
+       (window too small for the MQ to add the margin to body),
+       then auto-close the sidebar once you click on something in there. */
+    toc.addEventListener('click', function(e) {
+      if(e.target.tagName.toLowerCase() == "a" && document.body.classList.contains('toc-sidebar') && !sidebarMedia.matches) {
+        toggleSidebar(false);
+      }
+    }, false);
+  }
+  else {
+    console.warn("Can't find Table of Contents. Please use <nav id='toc'> around the ToC.");
+  }
 
   /* Wrap tables in case they overflow */
   var tables = document.querySelectorAll(':not(.overlarge) > table.data, :not(.overlarge) > table.index');
   var numTables = tables.length;
   for (var i = 0; i < numTables; i++) {
     var table = tables[i];
-    var wrapper = document.createElement('div');
-    wrapper.className = 'overlarge';
-    table.parentNode.insertBefore(wrapper, table);
-    wrapper.appendChild(table);
+    if (!table.matches('.example *, .note *, .advisement *, .def *, .issue *')) {
+      /* Overflowing colored boxes looks terrible, and also
+         the kinds of tables inside these boxes
+         are less likely to need extra space. */
+      var wrapper = document.createElement('div');
+      wrapper.className = 'overlarge';
+      table.parentNode.insertBefore(wrapper, table);
+      wrapper.appendChild(table);
+    }
   }
 
 })();
+
